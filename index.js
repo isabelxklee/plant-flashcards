f84898eab5f53f08eaa0ec8ee45b991b646a0523
isabel k. lee <hello@isabelklee.co>
2020-05-02 19:26:52 -0400

    refactor flip card

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const body = document.querySelector("body")
 fetch(flashcardsURL)
     .then(r => r.json())
     .then((flashcardsArr) => {
-        renderFlashcard(flashcardsArr[0])
+        renderFrontFlashcard(flashcardsArr[0])
     })
 
-function renderFlashcard(flash) {
+function renderFrontFlashcard(flash) {
     let flashcardContainer = document.createElement("div")
     flashcardContainer.classList.add("flip-card")
 
@@ -18,60 +18,31 @@ function renderFlashcard(flash) {
     body.append(flashcardContainer)
     flashcardContainer.append(innerCard)
 
-    innerCard.innerHTML = ""
-
     let plantImage = document.createElement("img")
-    // plantImage.classList.add("flip-card-front")
+    plantImage.classList.add("flip-card-front")
     plantImage.src = flash.plant_image
 
     let plantName = document.createElement("h2")
     plantName.classList.add("flip-card-front")
     plantName.innerText = flash.plant_name
 
+    // slap it on the DOM
     innerCard.append(plantImage, plantName)
     flashcardContainer.append(innerCard)
 
-    // flashcardContainer.addEventListener("click", (event) => {
-    //     flipFlashcard(flash)
-    // })
+    renderBackFlashcard(flash)
 }
 
-// <div class="flip-card">
+function renderBackFlashcard(flash) {
+    let innerCard = body.querySelector(".flip-card-inner")
 
-//   <div class="flip-card-inner">
+    let factTitle = document.createElement("h4")
+    factTitle.classList.add("flip-card-back")
+    factTitle.innerText = flash.fact_title
 
-//     <div class="flip-card-front">
-//       {/* plant image */}
-//       {/* plant name */}
-//     </div>
+    let factContent = document.createElement("p")
+    factContent.classList.add("flip-card-back")
+    factContent.innerText = flash.fact_content
 
-//     <div class="flip-card-back">
-//       {/* fact title */}
-//       {/* fact content */}
-//     </div>
-
-//   </div>
-
-// </div>
-
-// function flipFlashcard(flash) {
-//     console.log(flash)
-
-//     let flashcardContainer = body.querySelector(".card")
-
-//     flashcardContainer.innerHTML = ""
-
-//     let factTitle = document.createElement("h4")
-//     factTitle.classList.add("info-container")
-//     factTitle.innerText = flash.fact_title
-
-//     let factContent = document.createElement("p")
-//     factContent.classList.add("info-container")
-//     factContent.innerText = flash.fact_content
-
-//     flashcardContainer.append(factTitle, factContent)
-
-//     flashcardContainer.addEventListener("click", (event) => {
-//         renderFlashcard(plant, flash)
-//     })
-// }
+    innerCard.append(factTitle, factContent)
+}

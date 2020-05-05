9a059c892073ff2b35fa6096b4d20c816d420791
isabel k. lee <hello@isabelklee.co>
2020-05-05 11:57:31 -0400

    trying to bring back prev and next button

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function loadFlashcardNavigation(flashcardsArr) {
     
     backButton.addEventListener("click", (event) => {
         indexPosition = indexPosition - 1
-        loadLearningMode(flashcardsArr[indexPosition])
+        loadFlashcard(flashcardsArr[indexPosition])
         cardCount.innerText = `${indexPosition + 1} / ${flashcardsArr.length} cards`
 
         console.log(`Index position: ${indexPosition}`)
@@ -27,7 +27,7 @@ function loadFlashcardNavigation(flashcardsArr) {
 
     nextButton.addEventListener("click", (event) => {
         indexPosition = indexPosition + 1
-        loadLearningMode(flashcardsArr[indexPosition])
+        loadFlashcard(flashcardsArr[indexPosition])
         cardCount.innerText = `${indexPosition + 1} / ${flashcardsArr.length} cards`
 
         console.log(`Index position: ${indexPosition}`)
@@ -35,47 +35,47 @@ function loadFlashcardNavigation(flashcardsArr) {
 }
 
 function loadLearningMode(flashcardsArr) {
+    loadFlashcard(flashcardsArr[0])
+    loadFlashcardNavigation(flashcardsArr)
+    navLinkActions()
+}
+
+function loadFlashcard(flash) {
     document.body.innerHTML = learningMode
     innerCard = document.querySelector("div.flip-card-inner")
-    let firstFlash = flashcardsArr[0]
 
     let plantImage = document.createElement("img")
     plantImage.classList.add("flip-card-front")
     plantImage.id = "plant-image"
-    plantImage.src = firstFlash.plant_image
+    plantImage.src = flash.plant_image
 
     let plantName = document.createElement("h2")
     plantName.classList.add("flip-card-front")
     plantName.id = "plant-name"
-    plantName.innerText = firstFlash.plant_name
+    plantName.innerText = flash.plant_name
 
     innerCard.append(plantImage, plantName)
 
     innerCard.addEventListener("click", (event) => {
         innerCard.innerHTML = ""
-        backCardInfo(flashcardsArr)
+        backCardInfo(flash)
     })
-
-    loadFlashcardNavigation(flashcardsArr)
-    navLinkActions()
 }
 
-function backCardInfo(flashcardsArr) {
-    let firstFlash = flashcardsArr[0]
-
+function backCardInfo(flash) {
     let emojiRating = document.createElement("p")
     emojiRating.classList.add("flip-card-back", "rating")
-    emojiRating.innerText = firstFlash.emoji_rating
+    emojiRating.innerText = flash.emoji_rating
 
     let lineBreak = document.createElement("br")
 
     let factTitle = document.createElement("h4")
     factTitle.classList.add("flip-card-back", "title")
-    factTitle.innerText = firstFlash.fact_title
+    factTitle.innerText = flash.fact_title
 
     let factContent = document.createElement("p")
     factContent.classList.add("flip-card-back", "content")
-    factContent.innerText = firstFlash.fact_content
+    factContent.innerText = flash.fact_content
 
     let flashcardContainer = body.querySelector(".flip-card")
     let innerCard = flashcardContainer.querySelector(".flip-card-inner")
@@ -84,7 +84,7 @@ function backCardInfo(flashcardsArr) {
 
     innerCard.addEventListener("click", (event) => {
         innerCard.innerHTML = ""
-        loadLearningMode(flashcardsArr)
+        loadFlashcard(flash)
     })
 }
 

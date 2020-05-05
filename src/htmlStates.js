876666c615a165610735dc7ed2cfe4240cb2b50f
isabel k. lee <hello@isabelklee.co>
2020-05-05 17:08:42 -0400

    laid out the HTML for the quiz section

--- src/htmlStates.js
+++ src/htmlStates.js
@@ -1,5 +1,4 @@
-// load a single flashcard
-// fill the inner card div with the correct flashcard information
+// persistent header throughout the entire user experience
 
 let header = `
 <div class="header">
@@ -10,23 +9,41 @@ let header = `
     </div>
 </div>`
 
+// load a single flashcard
+// fill the inner card div with the correct flashcard information
+
 let learningMode =
 `${header}
 
-<h1>Click on the card to flip it.</h1>
-<div class="flip-card">
-</div>
+<div class="card-intro">
+    <h1>Click on the card to flip it.</h1>
+    <div class="flip-card">
+    </div>
 
-<div id="flashcard-navigation">
-    <p id="card-count"></p>
-    <div class="btn-group">
-        <button class="navigation" id="back-button">Previous</button>
-        <button class="navigation" id="next-button">Next</button>
+    <div id="flashcard-navigation">
+        <p id="card-count"></p>
+        <div class="btn-group">
+            <button class="navigation" id="back-button">Previous</button>
+            <button class="navigation" id="next-button">Next</button>
+        </div>
     </div>
 </div>
 `
 
+// quiz game
+
 let quizTime = `
 ${header}
-<h1>Let's test your knowledge on plants!</h1>
+<div class="quiz-intro">
+    <p id="card-count"></p>
+
+    <h1>Which of these plants is pet-friendly?</h1>
+    <div class="answer-options">
+        <button>Monstera Plant</button>
+        <button>Snake Plant</button>
+        <button>ZZ Plant</button>
+    </div>
+
+    <h3 id="score">Score: </h3>
+</div>
 `

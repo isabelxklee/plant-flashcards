176dd6f41a9934845b7e908a221c40d64ab458a5
isabel k. lee <hello@isabelklee.co>
2020-05-05 11:39:43 -0400

    created learning mode and quiz time HTML states

--- htmlStates.js
+++ htmlStates.js
@@ -1,74 +1,33 @@
-let learningMode =
-`
+// load a single flashcard
+// fill the inner card div with the correct flashcard information
+
+let header = `
 <div class="header">
     <a href="#default" class="logo">🌿 Plant Flashcards 🌿</a>
     <div class="header-right">
-        <a href="#default">Learning mode</a>
-        <a href="#default">Quiz time</a>
+        <a href="#default" id="learning-mode">Learning mode</a>
+        <a href="#default" id="quiz-time">Quiz time</a>
+    </div>
+</div>`
+
+let learningMode =
+`${header}
+
+<h1>Click on the card to flip it.</h1>
+<div class="flip-card">
+    <div class="flip-card-inner">
+    </div>
+    
+    <p class="content" id="card-count"></p>
+
+    <div class="btn-group">
+        <button class="navigation" id="back-button">Previous</button>
+        <button class="navigation" id="next-button">Next</button>
     </div>
 </div>
 `
 
-// let gameHTML = `
-//   <h1 id="scramble"></h1>
-//   <h2>Time Left: </h2><h2 id="timer">60</h2>
-//   <div id="game-container">
-//     <div id="game-scores">
-//       <h2>Scores</h2>
-//       <table class="table is-hoverable is-narrow is-fullwidth" style="background-color:inherit">
-//         <thead>
-//           <tr>
-//             <td>Username</td>
-//             <td>Points</td>
-//           </tr>
-//         </thead>
-//         <tbody id="scoreboard">
-//         </tbody>
-//       </table>
-//     </div>
-//       <form id="submission-form">
-//         <div class="field has-addons">
-//           <div class="control">
-//             <input type="text" class="input is-large" id="submission" autocomplete="off">
-//           </div>
-//           <div class="control">
-//             <input id="submit-word" value="⏎" class="button is-primary is-large" type="submit">
-//           </div>
-//         </div>
-//       </form>
-//     <div id="attempts-container">
-//       <h2>Guessed Words</h2>
-//       <div id="word-attempts">
-//         <ul id="attempts-1">
-//         </ul>
-//       </div>
-//       <div id="word-attempts">
-//         <ul id="attempts-2">
-//         </ul>
-//       </div>
-//     </div>
-// </div>
-// `
-
-
-// let gameOverHTML = `
-//   <div id="winner">
-//     <h1> Your Name Here </h1>
-//   </div>
-//   <div id="final-scores">
-//     <h2>Final Scores</h2>
-//       <table class="table is-hoverable is-narrow is-fullwidth" style="background-color:inherit">
-//         <thead>
-//           <tr>
-//             <td>Username</td>
-//             <td>Points</td>
-//           </tr>
-//         </thead>
-//         <tbody id="scoreboard">
-//         </tbody>
-//       </table>
-//   </div>
-//   <div class="center-this">
-//     <button id="start-over" class="button is-primary is-large is-rounded">Play Again</button>
-//   </div>
-// `
+let quizTime = `
+${header}
+<h1>Let's test your knowledge on plants!</h1>
+`
--- index.js
+++ index.js
@@ -3,63 +3,23 @@ const body = document.querySelector("body")
 
 fetch(flashcardsURL)
     .then(r => r.json())
-    // .then((flashcardsArr) => {
-    //     loadLearningMode()
-    //     createCardContainer(flashcardsArr)
-    //     renderFrontFlashcard(flashcardsArr[0])
-    // })
-    .then(loadLearningMode())
+    .then((flashcardsArr) => {
+        loadLearningMode(flashcardsArr)
+        loadQuizTime()
+    })
 
-function renderFrontFlashcard(flash) {
-    frontCardInfo(flash)
-}
-
-function createCardContainer(flashcardsArr) {
-    let header = document.createElement("h1")
-    header.innerText = "Click on the card to flip it."
-
-    let cardCount = document.createElement("p")
-    cardCount.classList.add("content")
-    cardCount.id = "card-count"
+function loadFlashcardNavigation(flashcardsArr) {
+    let cardCount = document.getElementById("card-count")
     cardCount.innerText = `1 / ${flashcardsArr.length} cards`
-
-    let flashcardContainer = document.createElement("div")
-    flashcardContainer.classList.add("flip-card")
-
-    let innerCard = document.createElement("div")
-    innerCard.classList.add("flip-card-inner")
-
-    let buttonGroup = document.createElement("div")
-    buttonGroup.classList.add("btn-group")
-
-    let nextButton = document.createElement("button")
-    nextButton.classList.add("navigation")
-    nextButton.id = "next-button"
-    nextButton.innerText = "Next"
-
-    let backButton = document.createElement("button")
-    backButton.classList.add("navigation")
-    backButton.id = "back-button"
-    backButton.innerText = "Previous"
-
-    body.append(header, flashcardContainer)
-    buttonGroup.append(backButton, nextButton)
-    flashcardContainer.append(innerCard, cardCount, buttonGroup)
-
-    navigationActions(flashcardsArr)
-}
-
-function navigationActions(flashcardsArr) {
-    let flashcardContainer = body.querySelector(".flip-card")
-    let nextButton = flashcardContainer.querySelector("#next-button")
-    let backButton = flashcardContainer.querySelector("#back-button")
-    let cardCount = flashcardContainer.querySelector("#card-count")
+    console.log(cardCount)
 
     let indexPosition = 0
-
+    let backButton = document.getElementById("back-button")
+    let nextButton = document.getElementById("next-button")
+    
     backButton.addEventListener("click", (event) => {
         indexPosition = indexPosition - 1
-        renderFrontFlashcard(flashcardsArr[indexPosition])
+        loadLearningMode(flashcardsArr[indexPosition])
         cardCount.innerText = `${indexPosition + 1} / ${flashcardsArr.length} cards`
 
         console.log(`Index position: ${indexPosition}`)
@@ -67,49 +27,55 @@ function navigationActions(flashcardsArr) {
 
     nextButton.addEventListener("click", (event) => {
         indexPosition = indexPosition + 1
-        renderFrontFlashcard(flashcardsArr[indexPosition])
+        loadLearningMode(flashcardsArr[indexPosition])
         cardCount.innerText = `${indexPosition + 1} / ${flashcardsArr.length} cards`
 
         console.log(`Index position: ${indexPosition}`)
     })
 }
 
-function frontCardInfo(flash) {
+function loadLearningMode(flashcardsArr) {
+    document.body.innerHTML = learningMode
+    innerCard = document.querySelector("div.flip-card-inner")
+    let firstFlash = flashcardsArr[0]
+
     let plantImage = document.createElement("img")
     plantImage.classList.add("flip-card-front")
-    plantImage.src = flash.plant_image
+    plantImage.id = "plant-image"
+    plantImage.src = firstFlash.plant_image
 
     let plantName = document.createElement("h2")
     plantName.classList.add("flip-card-front")
-    plantName.innerText = flash.plant_name
-
-    let flashcardContainer = body.querySelector(".flip-card")
-    let innerCard = flashcardContainer.querySelector(".flip-card-inner")
-
-    innerCard.innerHTML = ""
+    plantName.id = "plant-name"
+    plantName.innerText = firstFlash.plant_name
 
     innerCard.append(plantImage, plantName)
 
     innerCard.addEventListener("click", (event) => {
         innerCard.innerHTML = ""
-        backCardInfo(flash)
+        backCardInfo(flashcardsArr)
     })
+
+    loadFlashcardNavigation(flashcardsArr)
+    navLinkActions()
 }
 
-function backCardInfo(flash) {
+function backCardInfo(flashcardsArr) {
+    let firstFlash = flashcardsArr[0]
+
     let emojiRating = document.createElement("p")
     emojiRating.classList.add("flip-card-back", "rating")
-    emojiRating.innerText = flash.emoji_rating
+    emojiRating.innerText = firstFlash.emoji_rating
 
     let lineBreak = document.createElement("br")
 
     let factTitle = document.createElement("h4")
     factTitle.classList.add("flip-card-back", "title")
-    factTitle.innerText = flash.fact_title
+    factTitle.innerText = firstFlash.fact_title
 
     let factContent = document.createElement("p")
     factContent.classList.add("flip-card-back", "content")
-    factContent.innerText = flash.fact_content
+    factContent.innerText = firstFlash.fact_content
 
     let flashcardContainer = body.querySelector(".flip-card")
     let innerCard = flashcardContainer.querySelector(".flip-card-inner")
@@ -118,10 +84,28 @@ function backCardInfo(flash) {
 
     innerCard.addEventListener("click", (event) => {
         innerCard.innerHTML = ""
-        frontCardInfo(flash)
+        loadLearningMode(flashcardsArr)
     })
 }
 
-function loadLearningMode() {
-    document.body.innerHTML = learningMode
+function loadQuizTime() {
+    navLinkActions()
+}
+
+function navLinkActions() {
+    let quizLink = document.getElementById("quiz-time")
+
+    quizLink.addEventListener("click", (event) => {
+        console.log("It's quiz time!")
+        body.innerHTML = quizTime
+        loadQuizTime(event)
+    })
+
+    let learningModeLink = document.getElementById("learning-mode")
+
+    learningModeLink.addEventListener("click", (event) => {
+        console.log("It's time to learn!")
+        body.innerHTML = learningMode
+        loadLearningMode()
+    })
 }

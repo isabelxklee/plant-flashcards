eb600d926fcd6e6d086c90fda9c2d19824ef3473
isabel k. lee <hello@isabelklee.co>
2020-09-16 14:57:30 -0400

    cleaned up indentations in quiz mode

--- src/quiz_mode.js
+++ src/quiz_mode.js
@@ -1,126 +1,121 @@
 function quizTimeLinkAction() {
-    let quizLink = document.getElementById("quiz-time")
+  let quizLink = document.getElementById("quiz-time")
 
-    quizLink.addEventListener("click", () => {
-        fetch(questionsURL)
-            .then(r => r.json())
-            .then((questionsArr) => {
-                loadQuizMode(questionsArr)
-                learningModeLinkAction()
-                scoreboardLinkAction()
-            })
+  quizLink.addEventListener("click", () => {
+    fetch(questionsURL)
+    .then(r => r.json())
+    .then((questionsArr) => {
+      loadQuizMode(questionsArr)
+      learningModeLinkAction()
+      scoreboardLinkAction()
     })
+  })
 }
 
 function loadQuizMode(questionsArr) {
-    document.body.innerHTML = quizTime
-    renderQuizElements(questionsArr)
-    quizTimeLinkAction()
+  document.body.innerHTML = quizTime
+  renderQuizElements(questionsArr)
+  quizTimeLinkAction()
 }
 
 function renderQuizElements(questionsArr) {
-    let answerOptions = document.querySelector(".answer-options")
-    let answerStatus = document.getElementById("status")
-    
-    let questionStatement = document.getElementById("question")
-    questionStatement.innerText = `${questionsArr[questionIndex].content}`
+  let answerOptions = document.querySelector(".answer-options")
+  let answerStatus = document.getElementById("status")
+  
+  let questionStatement = document.getElementById("question")
+  questionStatement.innerText = `${questionsArr[questionIndex].content}`
 
-    let nextQuestion = document.getElementById("next-question")
-    nextQuestion.classList.add("incorrect")
+  let nextQuestion = document.getElementById("next-question")
+  nextQuestion.classList.add("incorrect")
 
+  let quizIntro = document.querySelector(".page-container")
+  let numberOfQuestions = questionsArr.length
+  let questionCount = quizIntro.querySelector("#plant-count")
+  questionCount.innerText = `${questionIndex + 1} / ${numberOfQuestions} questions`
+
+  answerOptionLoop(questionsArr[questionIndex], questionsArr)
+
+  nextQuestion.addEventListener("click", () => {
     let quizIntro = document.querySelector(".page-container")
     let numberOfQuestions = questionsArr.length
     let questionCount = quizIntro.querySelector("#plant-count")
+
+    questionIndex = questionIndex + 1
+
+    answerOptions.innerHTML = ""
+    questionStatement.innerText = `${questionsArr[questionIndex].content}`
+
     questionCount.innerText = `${questionIndex + 1} / ${numberOfQuestions} questions`
 
+    answerStatus.innerText = ""
+
     answerOptionLoop(questionsArr[questionIndex], questionsArr)
-
-    nextQuestion.addEventListener("click", () => {
-        let quizIntro = document.querySelector(".page-container")
-        let numberOfQuestions = questionsArr.length
-        let questionCount = quizIntro.querySelector("#plant-count")
-
-        questionIndex = questionIndex + 1
-        console.log(`Question #${questionIndex + 1}`)
-
-        answerOptions.innerHTML = ""
-        questionStatement.innerText = `${questionsArr[questionIndex].content}`
-
-        questionCount.innerText = `${questionIndex + 1} / ${numberOfQuestions} questions`
-
-        answerStatus.innerText = ""
-
-        answerOptionLoop(questionsArr[questionIndex], questionsArr)
-    }) // end of continue button event listener
+  }) // end of continue button event listener
 } // end of function
 
 function answerOptionLoop(singleQuestion, questionsArr) {
-    let quizIntro = document.querySelector(".page-container")
+  let quizIntro = document.querySelector(".page-container")
 
-    let numberOfQuestions = questionsArr.length
-    let lastIndexPosition = numberOfQuestions - 1
+  let numberOfQuestions = questionsArr.length
+  let lastIndexPosition = numberOfQuestions - 1
 
-    let scoreKeeper = document.getElementById("score")
+  let scoreKeeper = document.getElementById("score")
+  
+  let answerOptions = document.querySelector(".answer-options")
+  let answerStatus = document.getElementById("status")
+
+  let nextQuestion = document.getElementById("next-question")
+  nextQuestion.classList.add("incorrect")
+
+  singleQuestion.answers.forEach((answer) => {    
+    let answerButton = document.createElement("button")
+    answerButton.innerText = answer.content
+    answerOptions.append(answerButton)
+
+    answerButton.addEventListener("click", (event) => {
+      if (answer.correct_answer === true) {
+        answerButton.classList.toggle("correct")
+
+        answerStatus.innerText = "Correct! 🎉"
+
+        scoreCount = scoreCount + 50
+        scoreKeeper.innerText = `Score: ${scoreCount}`
+
+        nextQuestion.classList.remove("incorrect")
+      } else {
+        answerStatus.innerText = "Wrong answer 😔"
+        answerButton.classList.toggle("incorrect")
+      }
+    }) // end of answer button event listener
+  }) // end of for each statement
+
+  if (singleQuestion === questionsArr[lastIndexPosition]) {
+    nextQuestion.remove()
+
+    let saveScore = document.createElement("button")
+    saveScore.classList.add("navigation")
+    saveScore.id = "save"
+    saveScore.innerText = "Save score"
     
-    let answerOptions = document.querySelector(".answer-options")
-    let answerStatus = document.getElementById("status")
+    quizIntro.append(saveScore)
 
-    let nextQuestion = document.getElementById("next-question")
-    nextQuestion.classList.add("incorrect")
-
-    singleQuestion.answers.forEach((answer) => {    
-        let answerButton = document.createElement("button")
-        answerButton.innerText = answer.content
-        answerOptions.append(answerButton)
-    
-        answerButton.addEventListener("click", (event) => {
-    
-            if (answer.correct_answer === true) {
-                console.log("This is correct!")
-                answerButton.classList.toggle("correct")
-    
-                answerStatus.innerText = "Correct! 🎉"
-    
-                scoreCount = scoreCount + 50
-                scoreKeeper.innerText = `Score: ${scoreCount}`
-    
-                nextQuestion.classList.remove("incorrect")
-            } else {
-                console.log("WRONG!")
-                answerStatus.innerText = "Wrong answer 😔"
-                answerButton.classList.toggle("incorrect")
-            }
-        }) // end of answer button event listener
-    }) // end of for each statement
-
-    if (singleQuestion === questionsArr[lastIndexPosition]) {
-        nextQuestion.remove()
-
-        let saveScore = document.createElement("button")
-        saveScore.classList.add("navigation")
-        saveScore.id = "save"
-        saveScore.innerText = "Save score"
-        
-        quizIntro.append(saveScore)
-
-        saveScore.addEventListener("click", () => {
-            loadAllPlayers()
-        })
-    } 
+    saveScore.addEventListener("click", () => {
+      loadAllPlayers()
+    })
+  } 
 }
 
-///////////////////////////////////
 function redirectToQuiz() {
-    let takeQuizButton = document.getElementById("take-quiz")
-    takeQuizButton.style.display = "block"
+  let takeQuizButton = document.getElementById("take-quiz")
+  takeQuizButton.style.display = "block"
 
-    takeQuizButton.addEventListener("click", () => {
-        fetch(questionsURL)
-            .then(r => r.json())
-            .then((questionsArr) => {
-                loadQuizMode(questionsArr)
-                learningModeLinkAction()
-                scoreboardLinkAction()
-            })
+  takeQuizButton.addEventListener("click", () => {
+    fetch(questionsURL)
+    .then(r => r.json())
+    .then((questionsArr) => {
+      loadQuizMode(questionsArr)
+      learningModeLinkAction()
+      scoreboardLinkAction()
     })
+  })
 }
